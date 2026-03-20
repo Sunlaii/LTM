500385a06e44f31eb0085242b41ed7533ae8be3b
Minh Nhat Hua <sunlight21@icloud.com>
2026-03-20 13:48:03 +0700

    Initial commit

---
+++ week7/W5_Server.java
@@ -0,0 +1,59 @@
+package week7;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class W5_Server {
+    private int port;
+
+    // Constructor
+    public W5_Server(int port) {
+        this.port = port;
+    }
+
+    // Khởi tạo server, tạo đối tượng socket tương ứng từng client
+    public void start() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            while (true) {
+                System.out.println("Server đang lắng nghe tại port " + port);
+                Socket socket = server.accept();
+                handleClient(socket);
+            }
+        } catch (IOException e) {
+            System.err.println("Lỗi khởi tạo server socket: " + e.getMessage());
+        }
+    }
+
+    // Xử lý khi có client kết nối
+    private void handleClient(Socket socket) {
+        System.out.println("Đã chấp nhận kết nối từ client: " + socket.getRemoteSocketAddress());
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
+            String dataFromClient;
+            while ((dataFromClient = reader.readLine()) != null) {
+                System.out.println("Server nhận: " + dataFromClient);
+                if (dataFromClient.equalsIgnoreCase("bye")) {
+                    System.out.println("Server nhận yêu cầu đóng kết nối từ client.");
+                    break;
+                }
+                String response = processData(dataFromClient);
+                writer.println(response);
+                writer.println("<END>"); // Báo client biết đã kết thúc gửi dữ liệu.
+            }
+        } catch (IOException e) {
+            System.err.println("Lỗi kết nối từ client: " + e.getMessage());
+        }
+    }
+
+    // Xử lý dữ liệu
+    private String processData(String input) {
+        StringBuilder response = new StringBuilder(input);
+        return "Server phản hồi: " + response.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        W5_Server server = new W5_Server(12345);
+        server.start();
+    }
+}
